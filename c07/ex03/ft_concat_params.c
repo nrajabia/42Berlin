7a442d48d786e5214935c03bec44360f57a22a5f
nrajabia <113427848+nrajabia@users.noreply.github.com>
2022-09-19 15:02:45 +0200

    second in uni for first day

---
+++ c07/ex03/ft_concat_params.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *ft_concat_params(int argc, char **argv)
+{
+	char *str;
+	char *ptr;
+	int couner;
+	int strlen;
+
+	counter = -1;
+	strlen = -1;
+	while(++counter < argc)
+	{
+		while(*argv[counter] != '\0')
+			strlen++;
+		for
+		
+	}
+}
+
+int main(int ac; char **av)
+{
+	printf("%s", ft_concat_params(ac, av));
+	return(0);
+}
