c1c7227d2acf182254d4f63508e4d46de19ade89
nrajabia <113427848+nrajabia@users.noreply.github.com>
2022-09-19 15:05:07 +0200

    for assurance

---
+++ c07/ex02/ft_ultimste_range.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int **ft_ultimate_range(int **range, int min, int max)
+{
+        int num[max - min];
+        int *ptr;
+        int len;
+        int counter;
+
+        counter = 0;
+        len = max - min;
+        while(counter < len)
+        {
+                num[counter] = min;
+                counter++;
+                min++;
+        }
+        ptr = (int *)(malloc(sizeof(int) * len));
+        ptr = num;
+	range = &ptr;
+        return(range);
+}
+
+int main()
+{
+        int i;
+	int **range;
+	
+	printf("length is %lu\n", sizeof(ft_ultimate_range(range,10, 18)));
+        for(i = 0; i < 8; i++)
+                printf("%d\n", ft_ultimate_range(range,10, 18)[0][i]);
+        return(0);
+} 
