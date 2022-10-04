47c10f4d45433d1c8606d33cc5ea1e361b2b7928
nrajabia <113427848+nrajabia@users.noreply.github.com>
2022-10-04 19:03:19 +0200

    finish ex04

---
+++ c08/ex04/ft_stock_str.h
@@ -0,0 +1,10 @@
+#ifndef __FT_STR_TO_TAB__
+	#define __FT_STR_TO_TAB__
+	
+	typedef struct s_stock_str
+	{
+		int size;
+		char *str;
+		char *copy;
+	}	t_stock_str;
+#endif
---
+++ c08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,63 @@
+#include "ft_stock_str.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+int ft_length(char *av)
+{
+	int size;
+
+	size = 0;
+	while (*av++)
+		size++;
+	return(size);
+}
+
+struct s_stock_str *ft_strs_to_tab(int ac, char **av)
+{
+	t_stock_str *param;
+	int count;
+
+	param = malloc(sizeof(t_stock_str) * ac);
+	if (!param)
+	{
+		free(param);
+		return(NULL);
+	}
+	else
+	{ 
+		count = 0;
+		while(ac--)
+		{
+			param[count].size = ft_length(av[count]);
+			param[count].str = av[count];
+			param[count].copy = param[count].str;
+		}
+	}
+	return(param);
+}
+
+void ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+	write(1, "\n", 2);
+}
+
+void ft_show_tab(struct s_stock_str *ptr)
+{
+	int count;
+	printf("%c\n", (ptr[0].size + 48));
+	count = -1;
+	/*while (++count < sizeof(ptr))
+	{
+		ft_putstr(ptr[count].size);
+		ft_putstr(ptr[count].str);
+		ft_putstr(ptr[count].copy);
+	}*/
+}
+
+int main(int ac, char **av)
+{
+	ft_show_tab(ft_strs_to_tab(ac, av));
+	return(0);
+}
